19a062b0a8d69bc093edad4bae25ca7bced4a994
Stax124 <tamoncz@gmail.com>
2022-07-12 21:30:20 +0200

    Enable asset folder as static file dir for fastapi

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
-import sys
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from sqlmodel.sql.expression import Select, SelectOfScalar
+from starlette.responses import FileResponse
 
 from db import generate_engine, get_session
 from routes import config, guild
@@ -15,3 +16,14 @@ Select.inherit_cache = True  # type: ignore
 
 app.include_router(config.router)
 app.include_router(guild.router)
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
+
+@app.get("/")
+async def index():
+    return FileResponse("index.html")
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse("favicon.ico")
